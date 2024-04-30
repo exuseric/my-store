1746552f9a71b3753b2ed94a925237e130e344cf
Eric Maina <70095144+exuseric@users.noreply.github.com>
2024-04-30 19:43:45 +0300

    Update key value

--- app/components/productGrid/index.js
+++ app/components/productGrid/index.js
@@ -4,8 +4,8 @@ import ProductCardGrid from "../productCardGrid";
 export default function productGrid({ column_count, products }) {
     return (
         <Grid columns={{ initial: '1', md: `repeat(${column_count}, 1fr)` }} rows='repeat(auto, 1fr)' gap={'3'} py={'7'} align={'center'} justify={'between'}>
-            {products?.map((product, idx) => (
-                <ProductCardGrid product={product} key={idx} />
+            {products?.map((product, index) => (
+                <ProductCardGrid product={product} key={index} />
             ))}
         </Grid>
     )
