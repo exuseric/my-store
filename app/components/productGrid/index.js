5e9b1a9107e6095a9dc9780aee6b743a6e761f26
Eric Maina <70095144+exuseric@users.noreply.github.com>
2024-04-30 19:47:21 +0300

    Update key value to use product id.

--- app/components/productGrid/index.js
+++ app/components/productGrid/index.js
@@ -4,8 +4,8 @@ import ProductCardGrid from "../productCardGrid";
 export default function productGrid({ column_count, products }) {
     return (
         <Grid columns={{ initial: '1', md: `repeat(${column_count}, 1fr)` }} rows='repeat(auto, 1fr)' gap={'3'} py={'7'} align={'center'} justify={'between'}>
-            {products?.map((product, index) => (
-                <ProductCardGrid product={product} key={index} />
+            {products?.map(product => (
+                <ProductCardGrid product={product} key={product.id} />
             ))}
         </Grid>
     )
